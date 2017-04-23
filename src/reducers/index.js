bf964376fcc866c458958df53fef68361466d8b9
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-23 21:57:22 +0700

    add: Reducer Composition with combineReducers()

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,10 @@
+import { combineReducers } from 'redux';
 import items from './items';
 import visibilityFilter from './visibilityFilter';
 
-const ngaji = (
-  state = {},
-  action,
-) => ({
-  items: items(
-    state.items,
-    action
-  ),
-  visibilityFilter: visibilityFilter(
-    state.visibilityFilter,
-    action
-  )
+const ngaji = combineReducers({
+  items,
+  visibilityFilter
 });
 
 export default ngaji;
