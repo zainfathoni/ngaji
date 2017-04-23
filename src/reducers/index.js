390da827636ea45e76ff3572a34f508cc4485386
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-23 22:09:44 +0700

    refactor: Implementing combineReducers() from Scratch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,21 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 import items from './items';
 import visibilityFilter from './visibilityFilter';
+
+const combineReducers = reducers => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        );
+        return nextState;
+      },
+      {}
+    )
+  }
+}
 
 const ngaji = combineReducers({
   items,
