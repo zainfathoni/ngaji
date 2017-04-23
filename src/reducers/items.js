b9533beac93272394521646cc74b2450ac420dc7
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-23 22:51:53 +0700

    refactor: Clear NPM Errors and Warnings

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,3 +1,43 @@
+const item = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return {
+        id: action.id,
+        activity: action.activity,
+        target: action.target,
+        unit: action.unit,
+        enabled: true
+      };
+    case 'RENAME_ACTIVITY':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        activity: action.activity
+      };
+    case 'UPDATE_TARGET':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        target: action.target || state.target,
+        unit: action.unit || state.unit,
+      };
+    case 'TOGGLE_ITEM':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        enabled: !state.enabled
+      };
+    default:
+      return state;
+  }
+}
+
 const items = (state = [], action) => {
   switch (action.type) {
     case 'ADD_ITEM':
@@ -13,48 +53,5 @@ const items = (state = [], action) => {
       return state;
   }
 };
-
-const item = (state, action) => {
-  switch (action.type) {
-    case 'ADD_ITEM':
-      return {
-        id: action.id,
-        activity: action.activity,
-        target: action.target,
-        unit: action.unit,
-        enabled: true
-      };
-    case 'RENAME_ACTIVITY':
-      if (state.id !== action.id) {
-        return state;
-      } else {
-        return {
-          ...state,
-          activity: action.activity
-        }
-      };
-    case 'UPDATE_TARGET':
-      if (state.id !== action.id) {
-        return state;
-      } else {
-        return {
-          ...state,
-          target: action.target || state.target,
-          unit: action.unit || state.unit,
-        }
-      };
-    case 'TOGGLE_ITEM':
-      if (state.id !== action.id) {
-        return state;
-      } else {
-        return {
-          ...state,
-          enabled: !state.enabled
-        }
-      };
-    default:
-      return state;
-  }
-}
 
 export default items;
