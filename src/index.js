68f129d775ee546f02d5cd25aedcf956e235c627
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-25 15:46:56 +0700

    refactor: Implementing Store from Scratch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import Ngaji from './components/Ngaji';
 import ngaji from './reducers';
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
 
 const store = createStore(ngaji);
 const render = () => {
