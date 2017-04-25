e9ff934607c6f1eda88b3c06acba94273f37dfa7
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-25 09:17:10 +0700

    refactor: Generating Containers with connect() from React Redux (FilterLink)

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,45 +1,34 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Link from './Link';
 
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;   
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;   
-    const state = store.getState();
-    
-    return (
-      <Link
-        active={
-          props.filter ===
-          state.visibilityFilter
-        }
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }
-      >
-        {props.children}
-      </Link>
-    )
-  }
+const mapStateToProps = (
+  state,
+  ownProps
+) => {
+  return {
+    active:
+      ownProps.filter ===
+      state.visibilityFilter
+  };
 }
 
-FilterLink.contextTypes = {
-  store: PropTypes.object
+const mapDispatchToProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      });
+    }
+  };
 }
+
+const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link);
 
 export default FilterLink;
