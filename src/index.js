07f0851316dc1b79fd7375e9309586793cfb275a
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-25 08:38:26 +0700

    refactor: Passing the Store Down with <Provider> from React Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Ngaji from './components/Ngaji';
-import Provider from './components/Provider';
 import ngaji from './reducers';
 
 const render = () => {
