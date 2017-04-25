e98556ad8e5c286b4833b89f7aa0a268b98fd9bc
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-25 17:49:54 +0700

    add: Supplying the Initial State

--- src/index.js
+++ src/index.js
@@ -5,9 +5,30 @@ import { Provider } from 'react-redux';
 import Ngaji from './components/Ngaji';
 import ngaji from './reducers';
 
+const persistedState = {
+  items: [
+    {
+      id: 0,
+      activity: 'Tilawah',
+      target: 1,
+      unit: 'Juz',
+      enabled: false
+    },
+    {
+      id: 1,
+      activity: 'Shalat Dhuha',
+      target: 4,
+      unit: "Raka'at",
+      enabled: true
+    }
+  ]
+}
+
+const store = createStore(ngaji, persistedState);
+
 const render = () => {
   ReactDOM.render(
-    <Provider store={createStore(ngaji)}>
+    <Provider store={store}>
       <Ngaji />
     </Provider>,
     document.getElementById('root')
