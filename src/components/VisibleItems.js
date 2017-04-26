f21924ea654d861a7e66c5ed2e73051fd5c13233
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-26 14:02:13 +0700

    add: Fetching Data on Route Change

--- src/components/VisibleItems.js
+++ src/components/VisibleItems.js
@@ -1,23 +1,46 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Items from './Items';
 import { toggleItem } from '../actions';
 import { getVisibleItems } from '../reducers';
+import { fetchItems } from '../api';
+
+class VisibleItems extends Component {
+  componentDidMount() {
+    fetchItems(this.props.filter).then(items =>
+      console.log(this.props.filter, items)
+    );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchItems(this.props.filter).then(items =>
+        console.log(this.props.filter, items)
+      );
+    }
+  }
+
+  render() {
+    return (
+      <Items {...this.props} />
+    );
+  }
+}
 
 const mapStateToProps = (state, { match }) => {
+  const filter = match.params.filter || 'all';
   return {
-    items: getVisibleItems(
-      state,
-      match.params.filter || 'all'
-    )
+    items: getVisibleItems(state, filter),
+    filter
   };
 }
 
-const VisibleItems = withRouter(
+VisibleItems = withRouter(
   connect(
     mapStateToProps,
     { onItemClick: toggleItem }
-  )(Items)
+  )(VisibleItems)
 );
 
 export default VisibleItems;
