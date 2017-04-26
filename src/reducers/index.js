1d35f45d1d38a9b96e287c736558f2ca935f65dc
Zain Fathoni <zain.fathoni@bukalapak.com>
2017-04-26 07:58:01 +0700

    remove: visibilityFilter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import items from './items';
-import visibilityFilter from './visibilityFilter';
 
 const ngaji = combineReducers({
   items,
-  visibilityFilter
 });
 
 export default ngaji;
